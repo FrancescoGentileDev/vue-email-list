c29e01a020f261357121407c138060896ee50802
svalvastic <assogames2@gmail.com>
2022-09-20 15:31:18 +0200

    ✨ feat(script): basic resolve

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    emails: [],
+  },
+  mounted() {
+    for (let index = 0; index < 10; index++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
+        this.emails.push(response.data.response);
+      });
+    }
+  },
+});
